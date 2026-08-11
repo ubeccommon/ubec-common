9a5363db93eae94d17171f7a8e941d087d0472cc
Farmer <living-labs@ubec.network>
2026-08-11 09:37:24 +0000

    erdpuls: deployed from erdpuls_dashboard; untrack scaffold path

--- erdpuls/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Package: models
-Service: erdpuls
-License: GNU AGPL v3.0
-
-This project uses the services of Claude and Anthropic PBC.
-"""
--- erdpuls/tests/test_erdpuls.py
+++
@@ -1,9 +0,0 @@
-"""
-Tests: erdpuls
-License: GNU AGPL v3.0
-"""
-# TODO: implement tests
-
-
-def test_placeholder() -> None:
-    assert True
